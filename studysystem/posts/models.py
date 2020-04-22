68e179f07e557777b597d4934323ceed3f9ea277
Oxy <anoschenko.ov@phystech.edu>
2020-04-22 20:41:07 +0300

    add posts.models

--- studysystem/posts/models.py
+++ studysystem/posts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Posts(models.Model):
+    topic = models.TextField(verbose_name='Тема поста')
+    author = models.ForeignKey('users.Teacher', on_delete=models.CASCADE, verbose_name="id пользователя")
+    content = models.TextField(verbose_name='Текст поста')
+    added_at = models.DateTimeField(verbose_name='Время отправки', null=False, auto_now=True)
+    for_class = models.ForeignKey('classes.Class', on_delete=models.CASCADE, verbose_name="id класса")
+    subject = models.ForeignKey('classes.Subject', on_delete=models.CASCADE, verbose_name="id предмета")
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+
+class Comments(models.Model):
+    post = models.ForeignKey('Chat', on_delete=models.CASCADE, verbose_name="id поста")
+    author = models.ForeignKey('users.User', on_delete=models.CASCADE, verbose_name="id пользователя")
+    content = models.TextField(verbose_name='Текст комментария')
+    added_at = models.DateTimeField(verbose_name='Время отправки', null=False, auto_now=True)
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
